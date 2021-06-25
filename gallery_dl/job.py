grave0x/gallery-dl

commit 559e592b0b37104b66bc229908087a0b27ae3297

use '_extractor' info in UrlJobs

gallery_dl/job.py
@@ -597,10 +597,16 @@ class UrlJob(Job):
             for url in kwdict["_fallback"]:
                 print("|", url)
 
-    def handle_queue(self, url, _):
-        try:
-            UrlJob(url, self, self.depth + 1).run()
-        except exception.NoExtractorError:
+    def handle_queue(self, url, kwdict):
+        cls = kwdict.get("_extractor")
+        if cls:
+            extr = cls.from_url(url)
+        else:
+            extr = extractor.find(url)
+
+        if extr:
+            self.status |= self.__class__(extr, self).run()
+        else:
             self._write_unsupported(url)
 
 

test/test_job.py
@@ -104,11 +104,14 @@ test:child
 test:child
 """)
 
-    #  def test_child(self):
-        #  extr = TestExtractorParent.from_url("test:parent")
-        #  tjob = job.UrlJob(extr, depth=0)
-        #  self.assertEqual(self._capture_stdout(tjob), """\
-#  """)
+    def test_child(self):
+        extr = TestExtractorParent.from_url("test:parent")
+        tjob = job.UrlJob(extr, depth=0)
+        self.assertEqual(self._capture_stdout(tjob), 3 * """\
+https://example.org/1.jpg
+https://example.org/2.jpg
+https://example.org/3.jpg
+""")
 
 
 class TestInfoJob(TestJob):

commit 48fc0a215fbd594215d01e86296874730a2df990

fix depth counter in UrlJob
regression from 559e592b

It would either stop at the first level (-g) or go infinitely deep (-G)
Going down to for example level 3 with -ggg didn't work.

gallery_dl/job.py
@@ -626,7 +626,7 @@ class UrlJob(Job):
             extr = extractor.find(url)
 
         if extr:
-            self.status |= self.__class__(extr, self).run()
+            self.status |= self.__class__(extr, self, self.depth + 1).run()
         else:
             self._write_unsupported(url)
 