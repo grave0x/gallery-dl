grave0x/gallery-dl

commit 3a99077819a2aca5463a2571a18e7a3a2594d248

[cyberdrop] extract direct download URLs (#1943)
do not rely on redirects from f.cyberdrop.cc

gallery_dl/extractor/cyberdrop.py
@@ -8,7 +8,6 @@
 
 from .common import Extractor, Message
 from .. import text
-import binascii
 
 
 class CyberdropAlbumExtractor(Extractor):
@@ -19,7 +18,7 @@ class CyberdropAlbumExtractor(Extractor):
     archive_fmt = "{album_id}_{id}"
     pattern = r"(?:https?://)?(?:www\.)?cyberdrop\.me/a/([^/?#]+)"
     test = ("https://cyberdrop.me/a/keKRjm4t", {
-        "pattern": r"https://f\.cyberdrop\.cc/.*\.[a-z]+$",
+        "pattern": r"https://fs-\d+\.cyberdrop\.to/.*\.[a-z]+$",
         "keyword": {
             "album_id": "keKRjm4t",
             "album_name": "Fate (SFW)",
@@ -38,7 +37,14 @@ class CyberdropAlbumExtractor(Extractor):
     def items(self):
         url = self.root + "/a/" + self.album_id
         extr = text.extract_from(self.request(url).text)
-        extr("const albumData = {", "")
+
+        files = []
+        append = files.append
+        while True:
+            url = extr('downloadUrl: "', '"')
+            if not url:
+                break
+            append(text.unescape(url))
 
         data = {
             "album_id"   : self.album_id,
@@ -46,13 +52,11 @@ class CyberdropAlbumExtractor(Extractor):
             "date"       : text.parse_timestamp(extr("timestamp: ", ",")),
             "album_size" : text.parse_int(extr("totalSize: ", ",")),
             "description": extr("description: `", "`"),
+            "count"      : len(files),
         }
-        files = extr("fl: '", "'").split(",")
-        data["count"] = len(files)
 
         yield Message.Directory, data
-        for file_b64 in files:
-            file = binascii.a2b_base64(file_b64).decode()
-            text.nameext_from_url(file, data)
+        for url in files:
+            text.nameext_from_url(url, data)
             data["filename"], _, data["id"] = data["filename"].rpartition("-")
-            yield Message.Url, "https://f.cyberdrop.cc/" + file, data
+            yield Message.Url, url, data

commit d683fee8313e5c5db73269cb6d295e5bab7262f0

{cyberdrop] restore video extraction (fixes #1993)
fixes a regression introduced in 3a990778

gallery_dl/extractor/cyberdrop.py
@@ -17,18 +17,32 @@ class CyberdropAlbumExtractor(Extractor):
     directory_fmt = ("{category}", "{album_name} ({album_id})")
     archive_fmt = "{album_id}_{id}"
     pattern = r"(?:https?://)?(?:www\.)?cyberdrop\.me/a/([^/?#]+)"
-    test = ("https://cyberdrop.me/a/keKRjm4t", {
-        "pattern": r"https://fs-\d+\.cyberdrop\.to/.*\.[a-z]+$",
-        "keyword": {
-            "album_id": "keKRjm4t",
-            "album_name": "Fate (SFW)",
-            "album_size": 150069254,
-            "count": 62,
-            "date": "dt:2020-06-18 13:14:20",
-            "description": "",
-            "id": r"re:\w{8}",
-        },
-    })
+    test = (
+        # images
+        ("https://cyberdrop.me/a/keKRjm4t", {
+            "pattern": r"https://fs-\d+\.cyberdrop\.to/.*\.(jpg|png|webp)$",
+            "keyword": {
+                "album_id": "keKRjm4t",
+                "album_name": "Fate (SFW)",
+                "album_size": 150069254,
+                "count": 62,
+                "date": "dt:2020-06-18 13:14:20",
+                "description": "",
+                "id": r"re:\w{8}",
+            },
+        }),
+        # videos
+        ("https://cyberdrop.me/a/l8gIAXVD", {
+            "pattern": r"https://fs-\d+\.cyberdrop\.to/.*\.mp4$",
+            "count": 31,
+            "keyword": {
+                "album_id": "l8gIAXVD",
+                "album_name": "Achelois17 videos",
+                "album_size": 652037121,
+                "date": "dt:2020-06-16 15:40:44",
+            },
+        }),
+    )
 
     def __init__(self, match):
         Extractor.__init__(self, match)
@@ -41,7 +55,7 @@ class CyberdropAlbumExtractor(Extractor):
         files = []
         append = files.append
         while True:
-            url = extr('downloadUrl: "', '"')
+            url = extr('id="file" href="', '"')
             if not url:
                 break
             append(text.unescape(url))