grave0x/gallery-dl

commit 6f80811a2444327fcffbfa87fb918722cfd3b667

decouple extractor initialization
Introduce an 'initialize()' function that does the actual init
(session, cookies, config options) and can called separately from
the constructor __init__().

This allows, for example, to adjust config access inside a Job
before most of it already happened when calling 'extractor.find()'.

gallery_dl/extractor/deviantart.py
@@ -38,14 +38,18 @@ class DeviantartExtractor(Extractor):
 
     def __init__(self, match):
         Extractor.__init__(self, match)
+        self.user = match.group(1) or match.group(2)
+
+    def _init(self):
         self.flat = self.config("flat", True)
         self.extra = self.config("extra", False)
         self.original = self.config("original", True)
         self.comments = self.config("comments", False)
-        self.user = match.group(1) or match.group(2)
+
+        self.api = DeviantartOAuthAPI(self)
         self.group = False
         self.offset = 0
-        self.api = None
+        self._premium_cache = {}
 
         unwatch = self.config("auto-unwatch")
         if unwatch:
@@ -60,11 +64,13 @@ class DeviantartExtractor(Extractor):
             self._update_content = self._update_content_image
             self.original = True
 
-        self._premium_cache = {}
-        self.commit_journal = {
-            "html": self._commit_journal_html,
-            "text": self._commit_journal_text,
-        }.get(self.config("journals", "html"))
+        journals = self.config("journals", "html")
+        if journals == "html":
+            self.commit_journal = self._commit_journal_html
+        elif journals == "text":
+            self.commit_journal = self._commit_journal_text
+        else:
+            self.commit_journal = None
 
     def skip(self, num):
         self.offset += num
@@ -80,8 +86,6 @@ class DeviantartExtractor(Extractor):
             return True
 
     def items(self):
-        self.api = DeviantartOAuthAPI(self)
-
         if self.user and self.config("group", True):
             profile = self.api.user_profile(self.user)
             self.group = not profile
@@ -448,6 +452,9 @@ class DeviantartUserExtractor(DeviantartExtractor):
         }),
         ("https://shimoda7.deviantart.com/"),
     )
+
+    def initialize(self):
+        pass
 
     def items(self):
         base = "{}/{}/".format(self.root, self.user)

gallery_dl/extractor/furaffinity.py
@@ -28,6 +28,8 @@ class FuraffinityExtractor(Extractor):
     def __init__(self, match):
         Extractor.__init__(self, match)
         self.user = match.group(1)
+
+    def _init(self):
         self.offset = 0
 
         if self.config("descriptions") == "html":
@@ -383,6 +385,9 @@ class FuraffinityUserExtractor(FuraffinityExtractor):
             "count": 3,
         }),
     )
+
+    def initialize(self):
+        pass
 
     def items(self):
         base = "{}/{{}}/{}/".format(self.root, self.user)

commit df826bc47b6b753445dfe83488ef740fa258b241

fix --range causing crashes (#4557)
regression caused by 6f80811a

gallery_dl/extractor/deviantart.py
@@ -39,6 +39,7 @@ class DeviantartExtractor(Extractor):
     def __init__(self, match):
         Extractor.__init__(self, match)
         self.user = match.group(1) or match.group(2)
+        self.offset = 0
 
     def _init(self):
         self.flat = self.config("flat", True)
@@ -48,7 +49,6 @@ class DeviantartExtractor(Extractor):
 
         self.api = DeviantartOAuthAPI(self)
         self.group = False
-        self.offset = 0
         self._premium_cache = {}
 
         unwatch = self.config("auto-unwatch")
@@ -444,6 +444,8 @@ class DeviantartUserExtractor(DeviantartExtractor):
 
     def initialize(self):
         pass
+
+    skip = Extractor.skip
 
     def items(self):
         base = "{}/{}/".format(self.root, self.user)

gallery_dl/extractor/furaffinity.py
@@ -28,9 +28,9 @@ class FuraffinityExtractor(Extractor):
     def __init__(self, match):
         Extractor.__init__(self, match)
         self.user = match.group(1)
+        self.offset = 0
 
     def _init(self):
-        self.offset = 0
         self.external = self.config("external", False)
 
         if self.config("descriptions") == "html":
@@ -294,6 +294,8 @@ class FuraffinityUserExtractor(FuraffinityExtractor):
 
     def initialize(self):
         pass
+
+    skip = Extractor.skip
 
     def items(self):
         base = "{}/{{}}/{}/".format(self.root, self.user)

gallery_dl/job.py
@@ -106,16 +106,15 @@ class Job():
                 "current_git_head": util.git_head()
             }
 
-        # predicates
-        self.pred_url = self._prepare_predicates("image", True)
-        self.pred_queue = self._prepare_predicates("chapter", False)
-
     def run(self):
         """Execute or run the job"""
         extractor = self.extractor
         log = extractor.log
         msg = None
 
+        self._init()
+
+        # sleep before extractor start
         sleep = util.build_duration_func(
             extractor.config("sleep-extractor"))
         if sleep:
@@ -200,6 +199,11 @@ class Job():
             kwdict.pop(self.metadata_http, None)
         if self.kwdict:
             kwdict.update(self.kwdict)
+
+    def _init(self):
+        self.extractor.initialize()
+        self.pred_url = self._prepare_predicates("image", True)
+        self.pred_queue = self._prepare_predicates("chapter", False)
 
     def _prepare_predicates(self, target, skip=True):
         predicates = []
@@ -810,6 +814,8 @@ class DataJob(Job):
         self.filter = dict.copy if private else util.filter_dict
 
     def run(self):
+        self._init()
+
         extractor = self.extractor
         sleep = util.build_duration_func(
             extractor.config("sleep-extractor"))

test/results/deviantart.py
@@ -37,7 +37,7 @@ __tests__ = (
     "#category": ("", "deviantart", "gallery"),
     "#class"   : deviantart.DeviantartGalleryExtractor,
     "#pattern" : r"https://(images-)?wixmp-[^.]+\.wixmp\.com/f/.+/.+\.(jpg|png)\?token=.+",
-    "#count"   : ">= 30",
+    "#count"   : ">= 38",
 
     "allows_comments" : bool,
     "author"          : {
@@ -80,6 +80,17 @@ __tests__ = (
     "title"           : str,
     "url"             : r"re:https://www.deviantart.com/shimoda7/art/[^/]+-\d+",
     "username"        : "shimoda7",
+},
+
+{
+    "#url"     : "https://www.deviantart.com/shimoda7/gallery/",
+    "#comment" : "range/skip (#4557)",
+    "#category": ("", "deviantart", "gallery"),
+    "#class"   : deviantart.DeviantartGalleryExtractor,
+    "#options" : {"original": False},
+    "#pattern" : r"https://images-wixmp-[0-9a-f]+\.wixmp\.com/f/0e474835-ec35-4937-b647-b6830ed58bd1/d2idul-6158ded2-37ac-413d-802e-0689f0f020ad\.jpg\?token=[\w.]+",
+    "#range"   : "38-",
+    "#count"   : 1,
 },
 
 {

test/test_results.py
@@ -206,6 +206,7 @@ class ResultJob(job.DownloadJob):
             self.extractor.filename_fmt).format_map
 
     def run(self):
+        self._init()
         for msg in self.extractor:
             self.dispatch(msg)
 