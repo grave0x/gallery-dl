grave0x/gallery-dl

commit 6f80811a2444327fcffbfa87fb918722cfd3b667

decouple extractor initialization
Introduce an 'initialize()' function that does the actual init
(session, cookies, config options) and can called separately from
the constructor __init__().

This allows, for example, to adjust config access inside a Job
before most of it already happened when calling 'extractor.find()'.

gallery_dl/extractor/weibo.py
@@ -28,6 +28,8 @@ class WeiboExtractor(Extractor):
     def __init__(self, match):
         Extractor.__init__(self, match)
         self._prefix, self.user = match.groups()
+
+    def _init(self):
         self.retweets = self.config("retweets", True)
         self.videos = self.config("videos", True)
         self.livephoto = self.config("livephoto", True)
@@ -227,6 +229,9 @@ class WeiboUserExtractor(WeiboExtractor):
         ("https://m.weibo.cn/p/2304132314621010_-_WEIBO_SECOND_PROFILE_WEIBO"),
         ("https://www.weibo.com/p/1003062314621010/home"),
     )
+
+    def initialize(self):
+        pass
 
     def items(self):
         base = "{}/u/{}?tabtype=".format(self.root, self._user_id())

commit ba52fc3ed79f6dcc7c1eafd8f34abbac8ed440ca

[weibo] fix AttributeError in 'user' extractor (#5022)
yet another bug caused by 6f80811a

gallery_dl/extractor/weibo.py
@@ -225,9 +225,6 @@ class WeiboUserExtractor(WeiboExtractor):
     pattern = USER_PATTERN + r"(?:$|#)"
     example = "https://weibo.com/USER"
 
-    def initialize(self):
-        pass
-
     def items(self):
         base = "{}/u/{}?tabtype=".format(self.root, self._user_id())
         return self._dispatch_extractors((

test/results/weibo.py
@@ -13,7 +13,35 @@ __tests__ = (
     "#url"     : "https://weibo.com/1758989602",
     "#category": ("", "weibo", "user"),
     "#class"   : weibo.WeiboUserExtractor,
-    "#pattern" : r"^https://weibo\.com/u/1758989602\?tabtype=feed$",
+    "#urls"    : "https://weibo.com/u/1758989602?tabtype=feed",
+},
+
+{
+    "#url"     : "https://weibo.com/1758989602",
+    "#category": ("", "weibo", "user"),
+    "#class"   : weibo.WeiboUserExtractor,
+    "#options" : {"include": "all"},
+    "#urls"    : (
+        "https://weibo.com/u/1758989602?tabtype=home",
+        "https://weibo.com/u/1758989602?tabtype=feed",
+        "https://weibo.com/u/1758989602?tabtype=video",
+        "https://weibo.com/u/1758989602?tabtype=newVideo",
+        "https://weibo.com/u/1758989602?tabtype=album",
+    ),
+},
+
+{
+    "#url"     : "https://weibo.com/zhouyuxi77",
+    "#category": ("", "weibo", "user"),
+    "#class"   : weibo.WeiboUserExtractor,
+    "#urls"    : "https://weibo.com/u/7488709788?tabtype=feed",
+},
+
+{
+    "#url"     : "https://www.weibo.com/n/周于希Sally",
+    "#category": ("", "weibo", "user"),
+    "#class"   : weibo.WeiboUserExtractor,
+    "#urls"    : "https://weibo.com/u/7488709788?tabtype=feed",
 },
 
 {
@@ -69,9 +97,11 @@ __tests__ = (
     "#class"   : weibo.WeiboFeedExtractor,
     "#range"   : "1",
 
-    "status": {"user": {
-    "id"       : 7488709788,
-}},
+    "status": {
+        "user": {
+            "id": 7488709788,
+        },
+    },
 },
 
 {
@@ -80,9 +110,12 @@ __tests__ = (
     "#class"   : weibo.WeiboFeedExtractor,
     "#range"   : "1",
 
-    "status": {"user": {
-    "id"       : 7488709788,
-}},
+
+    "status": {
+        "user": {
+            "id": 7488709788,
+        },
+    },
 },
 
 {