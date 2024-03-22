grave0x/gallery-dl

commit de8c672f7207a43d07bab372744fd15ba5511e5f

[kemonoparty] add 'order-revisions' option (#5334)

gallery_dl/extractor/kemonoparty.py
@@ -40,6 +40,8 @@ class KemonopartyExtractor(Extractor):
     def _init(self):
         self.revisions = self.config("revisions")
         if self.revisions:
+            order = self.config("order-revisions")
+            self.revisions_reverse = order[0] in ("r", "a") if order else False
             self.revisions_unique = (self.revisions == "unique")
         self._prepare_ddosguard_cookies()
         self._find_inline = re.compile(
@@ -254,6 +256,9 @@ class KemonopartyExtractor(Extractor):
             rev["revision_count"] = cnt
             idx -= 1
 
+        if self.revisions_reverse:
+            revs.reverse()
+
         return revs
 
     def _revisions_all(self, url):
@@ -265,6 +270,9 @@ class KemonopartyExtractor(Extractor):
             rev["revision_index"] = idx
             rev["revision_count"] = cnt
             idx -= 1
+
+        if self.revisions_reverse:
+            revs.reverse()
 
         return revs
 

commit 971f8b27d6fb998263f60bdd909229316f40a088

[kemonoparty] fix exception for '/revision/' URLs
caused by de8c672f

gallery_dl/extractor/kemonoparty.py
@@ -40,9 +40,10 @@ class KemonopartyExtractor(Extractor):
     def _init(self):
         self.revisions = self.config("revisions")
         if self.revisions:
-            order = self.config("order-revisions")
-            self.revisions_reverse = order[0] in ("r", "a") if order else False
             self.revisions_unique = (self.revisions == "unique")
+        order = self.config("order-revisions")
+        self.revisions_reverse = order[0] in ("r", "a") if order else False
+
         self._prepare_ddosguard_cookies()
         self._find_inline = re.compile(
             r'src="(?:https?://(?:kemono|coomer)\.(?:su|party))?(/inline/[^"]+'