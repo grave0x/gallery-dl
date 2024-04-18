grave0x/gallery-dl

commit 34904fcf8648311e956d91a6bc5bece2dc4ae0ac

[common] add 'sleep-429' option (#5160)

gallery_dl/extractor/common.py
@@ -198,7 +198,10 @@ class Extractor():
                     if b'name="captcha-bypass"' in content:
                         self.log.warning("Cloudflare CAPTCHA")
                         break
-                if code not in retry_codes and code < 500:
+
+                if code == 429 and self._interval_429:
+                    pass
+                elif code not in retry_codes and code < 500:
                     break
 
             finally:
@@ -208,14 +211,18 @@ class Extractor():
             if tries > retries:
                 break
 
+            seconds = tries
             if self._interval:
-                seconds = self._interval()
-                if seconds < tries:
-                    seconds = tries
+                s = self._interval()
+                if seconds < s:
+                    seconds = s
+            if code == 429 and self._interval_429:
+                s = self._interval_429()
+                if seconds < s:
+                    seconds = s
+                self.wait(seconds=seconds, reason="429 Too Many Requests")
             else:
-                seconds = tries
-
-            self.sleep(seconds, "retry")
+                self.sleep(seconds, "retry")
             tries += 1
 
         raise exception.HttpError(msg, response)
@@ -292,6 +299,9 @@ class Extractor():
         self._interval = util.build_duration_func(
             self.config("sleep-request", self.request_interval),
             self.request_interval_min,
+        )
+        self._interval_429 = util.build_duration_func(
+            self.config("sleep-429", 60),
         )
 
         if self._retries < 0:

commit 6314011c4fbf0e65f236082798057c4fc36edb93

[common] fix NameError in Extractor.request()
… when accessing 'code' after an requests exception was raised.

Caused by the changes in 34904fcf8648311e956d91a6bc5bece2dc4ae0ac

gallery_dl/extractor/common.py
@@ -171,6 +171,7 @@ class Extractor():
                     requests.exceptions.ChunkedEncodingError,
                     requests.exceptions.ContentDecodingError) as exc:
                 msg = exc
+                code = 0
             except (requests.exceptions.RequestException) as exc:
                 raise exception.HttpError(exc)
             else: