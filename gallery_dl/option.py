grave0x/gallery-dl

commit 89b0173c297940bf52ffbc4d49d802ddf899b3f7

add '-4/--force-ipv4' and '-6/--force-ipv6' command-line options

gallery_dl/option.py
@@ -461,6 +461,17 @@ def build_parser():
         help="Client-side IP address to bind to",
     )
     networking.add_argument(
+        "-4", "--force-ipv4",
+        dest="source-address", nargs=0, action=ConfigConstAction,
+        const="0.0.0.0",
+        help="Make all connections via IPv4",
+    )
+    networking.add_argument(
+        "-6", "--force-ipv6",
+        dest="source-address", nargs=0, action=ConfigConstAction, const="::",
+        help="Make all connections via IPv6",
+    )
+    networking.add_argument(
         "--no-check-certificate",
         dest="verify", nargs=0, action=ConfigConstAction, const=False,
         help="Disable HTTPS certificate validation",

commit 866f792ef40adf5204c261cb72ef5d4690185deb

[options] fix passing negative numbers as arguments (#5262)
https://github.com/mikf/gallery-dl/issues/5262#issuecomment-2468677453

fixes regression introduced in 89b0173c

'argparse' sets a flag and changes its behavior when using something
that looks like a negative number as option string, '-4' and '-6' in
this case.

gallery_dl/option.py
@@ -803,4 +803,10 @@ def build_parser():
               "&& convert * ../doc.pdf\""),
     )
 
+    try:
+        # restore normal behavior when adding '-4' or '-6' as arguments
+        parser._has_negative_number_optionals.clear()
+    except Exception:
+        pass
+
     return parser