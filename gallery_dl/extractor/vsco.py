grave0x/gallery-dl

commit 07b862eb7e19d5b9e1c288a54e319d0b064ce8c7

[vsco] improve 'm3u8' handling

gallery_dl/extractor/vsco.py
@@ -38,9 +38,7 @@ class VscoExtractor(Extractor):
             if img["is_video"]:
                 if not videos:
                     continue
-                url = img["video_url"]
-                if not url.startswith("ytdl:"):
-                    url = "https://" + url
+                url = text.ensure_http_scheme(img["video_url"])
             else:
                 base = img["responsive_url"].partition("/")[2]
                 cdn, _, path = base.partition("/")
@@ -66,6 +64,7 @@ class VscoExtractor(Extractor):
                 "description": img.get("description") or "",
             })
             if data["extension"] == "m3u8":
+                url = "ytdl:" + url
                 data["_ytdl_manifest"] = "hls"
                 data["extension"] = "mp4"
             yield Message.Url, url, data

commit d1ac21595abbdde887644c66b7986a77cb080137

[vsco] fix 'video' extractor (#7113)
fixes regression introduced in 07b862eb7e19d5b9e1c288a54e319d0b064ce8c7

gallery_dl/extractor/vsco.py
@@ -322,7 +322,7 @@ class VscoVideoExtractor(VscoExtractor):
             "grid_name"     : "",
             "upload_date"   : media["createdDate"],
             "responsive_url": media["posterUrl"],
-            "video_url"     : "ytdl:" + media.get("playbackUrl"),
+            "video_url"     : media.get("playbackUrl"),
             "image_meta"    : None,
             "width"         : media["width"],
             "height"        : media["height"],

test/results/vsco.py
@@ -112,7 +112,7 @@ __tests__ = (
     "#url"     : "https://vsco.co/shavonec/video/c5eb34bb-dd13-4d7a-a09c-2a7cd719c9fa",
     "#category": ("", "vsco", "video"),
     "#class"   : vsco.VscoVideoExtractor,
-    "#pattern" : r"ytdl:https://stream\.mux\.com/3o01XbjqTI4rkeRwPzR17H9i7VSFdWX1h\.m3u8\?token=ey.+",
+    "#pattern" : r"^ytdl:https://stream\.mux\.com/3o01XbjqTI4rkeRwPzR17H9i7VSFdWX1h\.m3u8\?token=ey.+",
 
     "date"       : "dt:2020-02-04 15:11:52",
     "description": "Big news: 🎥 YOU CAN NOW POST VIDEOS TO VSCO ⚡️⚡️💪🏾. ⁣\n⁣\n🔗BTS from my #FashionIsActivism panel with the California African American Arts museum ✊🏾",