grave0x/gallery-dl

commit 0bafab1fd05ad41b50c60e9f4471dac65dfd3796

[zerochan] parse JSON-LD data (#7178)

gallery_dl/extractor/zerochan.py
@@ -64,16 +64,22 @@ class ZerochanExtractor(BooruExtractor):
 
     def _parse_entry_html(self, entry_id):
         url = "{}/{}".format(self.root, entry_id)
-        extr = text.extract_from(self.request(url).text)
+        page = self.request(url).text
 
+        try:
+            jsonld = self._extract_jsonld(page)
+        except Exception:
+            return {"id": entry_id}
+
+        extr = text.extract_from(page)
         data = {
             "id"      : text.parse_int(entry_id),
-            "author"  : text.parse_unicode_escapes(extr('    "name": "', '"')),
-            "file_url": extr('"contentUrl": "', '"'),
-            "date"    : text.parse_datetime(extr('"datePublished": "', '"')),
-            "width"   : text.parse_int(extr('"width": "', ' ')),
-            "height"  : text.parse_int(extr('"height": "', ' ')),
-            "size"    : text.parse_bytes(extr('"contentSize": "', 'B')),
+            "author"  : jsonld["author"]["name"],
+            "file_url": jsonld["contentUrl"],
+            "date"    : text.parse_datetime(jsonld["datePublished"]),
+            "width"   : text.parse_int(jsonld["width"][:-3]),
+            "height"  : text.parse_int(jsonld["height"][:-3]),
+            "size"    : text.parse_bytes(jsonld["contentSize"][:-1]),
             "path"    : text.split_html(extr(
                 'class="breadcrumbs', '</nav>'))[2:],
             "uploader": extr('href="/user/', '"'),
@@ -86,7 +92,7 @@ class ZerochanExtractor(BooruExtractor):
         tags = data["tags"] = []
         for tag in html.split("<li class=")[1:]:
             category = text.extr(tag, '"', '"')
-            name = text.extr(tag, 'data-tag="', '"')
+            name = text.unescape(text.extr(tag, 'data-tag="', '"'))
             tags.append(category.partition(" ")[0].capitalize() + ":" + name)
 
         return data

test/results/zerochan.py
@@ -250,4 +250,58 @@ __tests__ = (
     ],
 },
 
+{
+    "#url"     : "https://www.zerochan.net/4354955",
+    "#comment" : "quotes in HTML tags",
+    "#category": ("booru", "zerochan", "image"),
+    "#class"   : zerochan.ZerochanImageExtractor,
+    "#auth"    : False,
+    "#options" : {"metadata": False},
+
+    "tags": [
+        "Mangaka:Tory Patterson",
+        "Studio:SEGA",
+        "Game:Sonic Origins",
+        "Series:Sonic the Hedgehog",
+        "Character:Miles \"Tails\" Prower",
+        "Theme:Airplane",
+        "Theme:Flying",
+        "Theme:Fox",
+        "Source:Character Sheet",
+        "Source:Official Art",
+        "Source:Official Art from X",
+        "Source:X (Twitter)",
+    ],
+},
+
+{
+    "#url"     : "https://www.zerochan.net/2275437",
+    "#comment" : "unicode escapes with surrogate pair (#7178)",
+    "#category": ("booru", "zerochan", "image"),
+    "#class"   : zerochan.ZerochanImageExtractor,
+    "#auth"    : False,
+    "#options" : {"metadata": False},
+
+    "author"   : "MAYO🍚",
+    "date"     : "dt:2018-02-25 16:03:48",
+    "extension": "png",
+    "file_url" : "https://static.zerochan.net/Kongou.full.2275437.png",
+    "filename" : "Kongou.full.2275437",
+    "width"    : 1047,
+    "height"   : 1365,
+    "id"       : 2275437,
+    "size"     : 502784,
+    "source"   : "",
+    "uploader" : "SubaruSumeragi",
+    "path"     : [
+        "Kantai Collection",
+        "Kongou",
+    ],
+    "tags"     : [
+        "Mangaka:MAYO🍚",
+        "Game:Kantai Collection",
+        "Character:Kongou"
+    ],
+},
+
 )

commit 9ba7fb2a1c5ef457912cf8170e3b2e34335a07ab

[zerochan] fix "KeyError: 'author'" (#7282)
fixes regression introduced in 0bafab1fd05ad41b50c60e9f4471dac65dfd3796

gallery_dl/extractor/zerochan.py
@@ -74,7 +74,6 @@ class ZerochanExtractor(BooruExtractor):
         extr = text.extract_from(page)
         data = {
             "id"      : text.parse_int(entry_id),
-            "author"  : jsonld["author"]["name"],
             "file_url": jsonld["contentUrl"],
             "date"    : text.parse_datetime(jsonld["datePublished"]),
             "width"   : text.parse_int(jsonld["width"][:-3]),
@@ -87,6 +86,11 @@ class ZerochanExtractor(BooruExtractor):
             "source"  : text.unescape(text.remove_html(extr(
                 'id="source-url"', '</p>').rpartition("</s>")[2])),
         }
+
+        try:
+            data["author"] = jsonld["author"]["name"]
+        except Exception:
+            data["author"] = ""
 
         html = data["tags"]
         tags = data["tags"] = []

test/results/zerochan.py
@@ -304,4 +304,41 @@ __tests__ = (
     ],
 },
 
+{
+    "#url"     : "https://www.zerochan.net/4147104",
+    "#comment" : "no 'author' in JSON-LD data (#7282)",
+    "#category": ("booru", "zerochan", "image"),
+    "#class"   : zerochan.ZerochanImageExtractor,
+    "#auth"    : False,
+
+    "author"   : "",
+    "date"     : "dt:2024-04-02 12:09:30",
+    "extension": "jpg",
+    "file_url" : "https://static.zerochan.net/Lycoris.Recoil.full.4147104.jpg",
+    "filename" : "Lycoris.Recoil.full.4147104",
+    "width"    : 1061,
+    "height"   : 1500,
+    "id"       : 4147104,
+    "size"     : 224256,
+    "source"   : "https://twitter.com/animetv_jp/status/1775101399648374835/",
+    "uploader" : "cutesherry",
+    "path"     : [
+        "Lycoris Recoil",
+    ],
+    "tags"     : [
+        "Studio:A-1 Pictures",
+        "Series:Lycoris Recoil",
+        "Character:Inoue Takina",
+        "Character:Nishikigi Chisato",
+        "Theme:Bench",
+        "Theme:Cherry Tree",
+        "Theme:Floating Hair",
+        "Theme:Sitting On Bench",
+        "Theme:Sneakers",
+        "Theme:Spring",
+        "Source:Key Visual",
+        "Source:Official Art",
+    ],
+},
+
 )