grave0x/gallery-dl

commit 8c20b66cf89d4452e661f8fa3ee4ec0869f90e14

[pp:ugoira] preserve 'extension' when using "mode": "archive" (#7304)

gallery_dl/postprocessor/ugoira.py
@@ -156,12 +156,7 @@ class UgoiraPP(PostProcessor):
                     return self.log.debug("", exc_info=exc)
 
             if self.convert(pathfmt, tempdir):
-                if self.delete:
-                    pathfmt.delete = True
-                elif pathfmt.extension != "zip":
-                    self.log.info(pathfmt.filename)
-                    pathfmt.set_extension("zip")
-                    pathfmt.build_path()
+                pathfmt.delete = self.delete
 
     def convert_from_files(self, pathfmt):
         if not self._convert_files:

commit 49fe3a1473be6e532300c8016fc4adf8e17762f8

[pp:ugoira] restore 'keep-files' functionality (#7304)
fixes regression introduced in 8c20b66cf89d4452e661f8fa3ee4ec0869f90e14

gallery_dl/postprocessor/ugoira.py
@@ -135,6 +135,7 @@ class UgoiraPP(PostProcessor):
         if not self._convert_zip:
             return
         self._zip_source = True
+        ext = pathfmt.extension
 
         with self._tempdir() as tempdir:
             if tempdir:
@@ -153,7 +154,12 @@ class UgoiraPP(PostProcessor):
                     return self.log.debug("", exc_info=exc)
 
             if self.convert(pathfmt, tempdir):
-                pathfmt.delete = self.delete
+                if self.delete:
+                    pathfmt.delete = True
+                elif pathfmt.extension != ext:
+                    self.log.info(pathfmt.filename)
+                    pathfmt.set_extension(ext)
+                    pathfmt.build_path()
 
     def convert_from_files(self, pathfmt):
         if not self._convert_files: