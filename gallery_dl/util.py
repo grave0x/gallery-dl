grave0x/gallery-dl

commit 59deab135a44ca2f1cb8de6cd4fe711f57d41c4f

[util] add 're' helper

gallery_dl/util.py
@@ -32,6 +32,19 @@ try:
 except AttributeError:
     re_compile = re.sre_compile.compile
 
+CACHE_PATTERN = {}
+
+
+def re(pattern):
+    """Compile a regular expression pattern"""
+    try:
+        return CACHE_PATTERN[pattern]
+    except KeyError:
+        pass
+
+    p = CACHE_PATTERN[pattern] = re_compile(pattern)
+    return p
+
 
 def bencode(num, alphabet="0123456789"):
     """Encode an integer into a base-N encoded string"""
@@ -360,9 +373,8 @@ Response Headers
 
             set_cookie = res_headers.get("Set-Cookie")
             if set_cookie:
-                res_headers["Set-Cookie"] = re.sub(
-                    r"(^|, )([^ =]+)=[^,;]*", r"\1\2=***", set_cookie,
-                )
+                res_headers["Set-Cookie"] = re(r"(^|, )([^ =]+)=[^,;]*").sub(
+                    r"\1\2=***", set_cookie)
 
         fmt_nv = "{}: {}".format
 

test/test_util.py
@@ -12,6 +12,7 @@ import sys
 import unittest
 
 import io
+import re
 import time
 import random
 import string
@@ -973,6 +974,25 @@ value = 123
         self.assertEqual(response.cookies.get("foo"), None)
         self.assertEqual(response.headers.get("foo"), None)
         self.assertEqual(response.links.get("next"), None)
+
+    def test_re(self):
+        Pattern = type(re.compile(""))
+
+        p1 = util.re_compile("foo")
+        p2 = util.re("foo")
+        p3 = util.re("foo")
+
+        self.assertIsInstance(p1, Pattern)
+        self.assertIsInstance(p2, Pattern)
+        self.assertIsInstance(p3, Pattern)
+
+        self.assertIsNot(p1, p2)
+        self.assertIs(p2, p3)
+
+        if sys.hexversion >= 0x3060000:
+            self.assertEqual(p1, p2)
+        else:
+            self.assertEqual(repr(p1), repr(p2))
 
 
 class TestExtractor():

commit 73a97a31ef68a0adbb77eb4fd04501d16e8d5a32

[util] restore stdlib 're' module in filter expressions (#7665)
fixes regression introduced in 59deab135a44ca2f1cb8de6cd4fe711f57d41c4f

gallery_dl/util.py
@@ -8,7 +8,7 @@
 
 """Utility functions and classes"""
 
-import re
+import re as re_module
 import os
 import sys
 import json
@@ -28,9 +28,9 @@ from email.utils import mktime_tz, parsedate_tz
 from . import text, version, exception
 
 try:
-    re_compile = re._compiler.compile
+    re_compile = re_module._compiler.compile
 except AttributeError:
-    re_compile = re.sre_compile.compile
+    re_compile = re_module.sre_compile.compile
 
 CACHE_PATTERN = {}
 
@@ -791,7 +791,7 @@ GLOBALS = {
     "hash_sha1": sha1,
     "hash_md5" : md5,
     "std"      : ModuleProxy(),
-    "re"       : re,
+    "re"       : re_module,
     "exts_image"  : EXTS_IMAGE,
     "exts_video"  : EXTS_VIDEO,
     "exts_archive": EXTS_ARCHIVE,

test/test_util.py
@@ -161,6 +161,10 @@ class TestPredicate(unittest.TestCase):
 
         self.assertFalse(pred(url, {"a": 2}))
 
+        pred = util.FilterPredicate("re.search(r'.+', url)")
+        self.assertTrue(pred(url, {"url": "https://example.org/"}))
+        self.assertFalse(pred(url, {"url": ""}))
+
     def test_build_predicate(self):
         pred = util.build_predicate([])
         self.assertIsInstance(pred, type(lambda: True))