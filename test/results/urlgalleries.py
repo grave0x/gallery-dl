grave0x/gallery-dl

commit 183b8379add30f5b7e5d720401882a67f4dc422f

[urlgalleries] fix extraction (#7858)

gallery_dl/extractor/urlgalleries.py
@@ -37,12 +37,12 @@ class UrlgalleriesGalleryExtractor(GalleryExtractor):
         data = self.metadata(page)
         data["count"] = len(imgs)
 
-        root = "https://urlgalleries.net/b/" + blog
+        root = self.root
         yield Message.Directory, data
         for data["num"], img in enumerate(imgs, 1):
             page = self.request(root + img).text
             url = text.extr(page, "window.location.href = '", "'")
-            yield Message.Queue, url, data
+            yield Message.Queue, url.partition("?")[0], data
 
     def metadata(self, page):
         extr = text.extract_from(page)
@@ -53,7 +53,7 @@ class UrlgalleriesGalleryExtractor(GalleryExtractor):
             "_rprt": extr(' title="', '"'),  # report button
             "title": text.unescape(extr(' title="', '"').strip()),
             "date" : text.parse_datetime(
-                extr(" images in gallery | ", "<"), "%B %d, %Y %H:%M"),
+                extr(" images in gallery | ", "<"), "%B %d, %Y"),
         }
 
     def images(self, page):

test/results/urlgalleries.py
@@ -21,10 +21,10 @@ __tests__ = (
 
     "blog"      : "photos2q",
     "count"     : 39,
-    "date"      : "dt:2023-12-08 13:59:00",
+    "date"      : "dt:2023-12-08 00:00:00",
     "gallery_id": "7851311",
     "num"       : range(1, 3),
-    "title"     : "Clarice window 8",
+    "title"     : "Clarice window 8 g7rn2ebbao",
 },
 
 {
@@ -40,10 +40,10 @@ __tests__ = (
 
     "blog"      : "photos2q",
     "count"     : 39,
-    "date"      : "dt:2023-12-08 13:59:00",
+    "date"      : "dt:2023-12-08 00:00:00",
     "gallery_id": "7851311",
     "num"       : range(1, 3),
-    "title"     : "Clarice window 8",
+    "title"     : "Clarice window 8 g7rn2ebbao",
 },
 
 {
@@ -59,10 +59,10 @@ __tests__ = (
 
     "blog"      : "Dreamer",
     "count"     : 105,
-    "date"      : "dt:2020-03-10 21:17:00",
+    "date"      : "dt:2020-03-10 00:00:00",
     "gallery_id": "7645840",
     "num"       : range(1, 3),
-    "title"     : "Angelika - Rustic Charm - AmourAngels 2016-09-27",
+    "title"     : "Angelika - Rustic Charm - AmourAngels 2016-09-27 7722lhrmys",
 },
 
 )

commit 1e79d28555fa495c4db1099fc24af9a0c36dcec6

[urlgalleries] do not strip query parameters (#7888)
fixes regression introduced in 183b8379add30f5b7e5d720401882a67f4dc422f

gallery_dl/extractor/urlgalleries.py
@@ -42,7 +42,7 @@ class UrlgalleriesGalleryExtractor(GalleryExtractor):
         for data["num"], img in enumerate(imgs, 1):
             page = self.request(root + img).text
             url = text.extr(page, "window.location.href = '", "'")
-            yield Message.Queue, url.partition("?")[0], data
+            yield Message.Queue, url, data
 
     def metadata(self, page):
         extr = text.extract_from(page)

test/results/urlgalleries.py
@@ -14,9 +14,9 @@ __tests__ = (
     "#class"   : urlgalleries.UrlgalleriesGalleryExtractor,
     "#range"   : "1-3",
     "#results" : (
-        "https://fappic.com/x207mqkn2463/4gq1yv.jpg",
-        "https://fappic.com/q684ua2rp0j9/4gq1xv.jpg",
-        "https://fappic.com/8vf3n8fgz9po/4gq1ya.jpg",
+        "https://fappic.com/x207mqkn2463/4gq1yv.jpg?gname=Clarice+window+8",
+        "https://fappic.com/q684ua2rp0j9/4gq1xv.jpg?gname=Clarice+window+8",
+        "https://fappic.com/8vf3n8fgz9po/4gq1ya.jpg?gname=Clarice+window+8",
     ),
 
     "blog"      : "photos2q",
@@ -33,9 +33,9 @@ __tests__ = (
     "#class"   : urlgalleries.UrlgalleriesGalleryExtractor,
     "#range"   : "1-3",
     "#results" : (
-        "https://fappic.com/x207mqkn2463/4gq1yv.jpg",
-        "https://fappic.com/q684ua2rp0j9/4gq1xv.jpg",
-        "https://fappic.com/8vf3n8fgz9po/4gq1ya.jpg",
+        "https://fappic.com/x207mqkn2463/4gq1yv.jpg?gname=Clarice+window+8",
+        "https://fappic.com/q684ua2rp0j9/4gq1xv.jpg?gname=Clarice+window+8",
+        "https://fappic.com/8vf3n8fgz9po/4gq1ya.jpg?gname=Clarice+window+8",
     ),
 
     "blog"      : "photos2q",
@@ -63,6 +63,25 @@ __tests__ = (
     "gallery_id": "7645840",
     "num"       : range(1, 3),
     "title"     : "Angelika - Rustic Charm - AmourAngels 2016-09-27 7722lhrmys",
+},
+
+{
+    "#url"     : "https://urlgalleries.net/b/xarchivesx/porn-gallery-6722560/caroline",
+    "#comment" : "image host URLs with query parameters (#7888)",
+    "#class"   : urlgalleries.UrlgalleriesGalleryExtractor,
+    "#range"   : "1-3",
+    "#results" : (
+        "http://img272.imagevenue.com/img.php?image=63353_qedf2jsd4j_123_376lo.jpg",
+        "http://img220.imagevenue.com/img.php?image=63140_hl2kkhv0n4_123_621lo.jpg",
+        "http://img217.imagevenue.com/img.php?image=63140_z2edqlkpkz_123_986lo.jpg",
+    ),
+
+    "blog"      : "The Archives Blog",
+    "count"     : 141,
+    "date"      : "dt:2016-06-11 00:00:00",
+    "gallery_id": "6722560",
+    "num"       : range(1, 3),
+    "title"     : "Caroline q5c75eeybn",
 },
 
 )