grave0x/gallery-dl

commit 810e6841c577ac9092104ad74f861f147072f20c

[instagram] ensure '_ytdl_manifest_data' exists before DASH DL (#8267)

gallery_dl/extractor/instagram.py
@@ -95,7 +95,7 @@ class InstagramExtractor(Extractor):
                     if videos:
                         file["_http_headers"] = videos_headers
                         text.nameext_from_url(url, file)
-                        if videos_dash:
+                        if videos_dash and "_ytdl_manifest_data" in post:
                             file["_fallback"] = (url,)
                             file["_ytdl_manifest"] = "dash"
                             url = f"ytdl:{post['post_url']}{file['num']}.mp4"

commit 4e437e655826834a396b4980bc792f2a96942832

[instagram] restore 'video_dash_manifest' downloads (#8364)
fixes regression introduced in 810e6841c577ac9092104ad74f861f147072f20c

gallery_dl/extractor/instagram.py
@@ -100,7 +100,7 @@ class InstagramExtractor(Extractor):
                     if videos:
                         file["_http_headers"] = videos_headers
                         text.nameext_from_url(url, file)
-                        if videos_dash and "_ytdl_manifest_data" in post:
+                        if videos_dash and "_ytdl_manifest_data" in file:
                             file["_fallback"] = (url,)
                             file["_ytdl_manifest"] = "dash"
                             url = f"ytdl:{post['post_url']}{file['num']}.mp4"