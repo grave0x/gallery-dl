grave0x/gallery-dl

commit 208ff501a6809d85627c3ae52c19acd3f5e9872b

[kemono] improve 'filename'

test/results/kemono.py
@@ -137,7 +137,7 @@ __tests__ = (
         "https://kemono.cr/data/b0/38/b03882c8b0ab3b1cf9fc658a2bb2f9ac6ad4f3449015311dcd2d7ee7f748db31.png",
     ),
 
-    "tags": r"re:\bpin-up\b",
+    "tags": list,
 },
 
 {
@@ -241,7 +241,7 @@ __tests__ = (
     "#class"   : kemono.KemonoPostExtractor,
     "#options" : {"dms": True, "comments": True},
 
-    "comments": (),
+    "comments": [],
     "dms": [
         {
             "added"    : "2021-07-31T02:47:51.327865",
@@ -378,6 +378,7 @@ __tests__ = (
     "revision_hash": {
         "9872bfb536a47cc69d95d2f195cd5c825808f089",
         "e0e93281495e151b11636c156e52bfe9234c2a40",
+        "eb2fa4385af730509a42f8f0424bd0b9a0e4bc21",
     },
 },
 

gallery_dl/extractor/kemono.py
@@ -150,13 +150,19 @@ class KemonoExtractor(Extractor):
                     url = self.root + "/data" + url[20:]
                 file["url"] = url
 
-                text.nameext_from_url(file.get("name", url), file)
-                ext = text.ext_from_url(url)
-                if not file["extension"]:
-                    file["extension"] = ext
-                elif ext == "txt" and file["extension"] != "txt":
-                    file["_http_validate"] = _validate
-                elif ext in exts_archive or \
+                if name := file.get("name"):
+                    text.nameext_from_name(name, file)
+                    ext = text.ext_from_url(url)
+
+                    if not file["extension"]:
+                        file["extension"] = ext
+                    elif ext == "txt" and file["extension"] != "txt":
+                        file["_http_validate"] = _validate
+                else:
+                    text.nameext_from_url(url, file)
+                    ext = file["extension"]
+
+                if ext in exts_archive or \
                         ext == "bin" and file["extension"] in exts_archive:
                     file["type"] = "archive"
                     if archives:

commit 481be970846a20a9bb9df845b7f561c89cac161f

[text] reject long filename extensions (#8491)
fixes regression introduced in 208ff501a6809d85627c3ae52c19acd3f5e9872b
ref 5e89c45cb5c5b212c2215a627496583cefdbabb4

gallery_dl/text.py
@@ -125,7 +125,7 @@ def nameext_from_name(filename, data=None):
         data = {}
 
     name, _, ext = filename.rpartition(".")
-    if name:
+    if name and len(ext) <= 16:
         data["filename"] = name
         data["extension"] = ext.lower()
     else:

test/results/kemono.py
@@ -550,6 +550,17 @@ __tests__ = (
 },
 
 {
+    "#url"     : "https://kemono.cr/patreon/user/2570882/post/79311665",
+    "#comment" : "patreon file URL as 'name' / long 'extension' (#8491)",
+    "#category": ("", "kemono", "patreon"),
+    "#class"   : kemono.KemonoPostExtractor,
+
+    "name"     : "https://www.patreon.com/media-u/Z0FBQUFBQmpfWFNLWHpRakFlYjVNeWpuTlRuRnJBdHY3VVA2UmRhVHFpOFBHMW9QZUdVOHQ3b2pXSV9XMkJlaHFuN2JyVk5VNDBqdV9lZVRLR2NkUXUwSjgwdndDQlk3VzBCUXI5TW5iejlVWVZaUmJoTktIX3B5aGVCS3dUQk11a2hxajd4TUx2MFN2UHpKa0pfOWZQeS1UeDlzNEhpbG9pRzJsZE54MG5OcnZDOUllTGhyY01rNjVRaGgyaVFycjFSUUFIaV92OU9wdktuVjlMeFJNLXhYejdDNWZTVXZEc2l0TVZCR1A0YXM3RVMzbmsxSjh2ND0=#190833153_",
+    "filename" : "https://www.patreon.com/media-u/Z0FBQUFBQmpfWFNLWHpRakFlYjVNeWpuTlRuRnJBdHY3VVA2UmRhVHFpOFBHMW9QZUdVOHQ3b2pXSV9XMkJlaHFuN2JyVk5VNDBqdV9lZVRLR2NkUXUwSjgwdndDQlk3VzBCUXI5TW5iejlVWVZaUmJoTktIX3B5aGVCS3dUQk11a2hxajd4TUx2MFN2UHpKa0pfOWZQeS1UeDlzNEhpbG9pRzJsZE54MG5OcnZDOUllTGhyY01rNjVRaGgyaVFycjFSUUFIaV92OU9wdktuVjlMeFJNLXhYejdDNWZTVXZEc2l0TVZCR1A0YXM3RVMzbmsxSjh2ND0=#190833153_",
+    "extension": "jpg",
+},
+
+{
     "#url"     : "https://kemono.cr/discord/server/488668827274444803/608504710906904576",
     "#category": ("", "kemono", "discord"),
     "#class"   : kemono.KemonoDiscordExtractor,

test/test_text.py
@@ -228,6 +228,10 @@ class TestText(unittest.TestCase):
             {"filename": "foo%202?bar&<>", "extension": "ext"},
         )
 
+        # long "extension"
+        fn = "httpswww.example.orgpath-path-path-path-path-path-path-path"
+        self.assertEqual(f(fn), {"filename": fn, "extension": ""})
+
     def test_extract(self, f=text.extract):
         txt = "<a><b>"
         self.assertEqual(f(txt, "<", ">"), ("a" , 3))