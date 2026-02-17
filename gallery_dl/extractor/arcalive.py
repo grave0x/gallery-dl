grave0x/gallery-dl

commit c0eddedeae7b382dd2a29a266a499dc98a7b0c1e

[common] add reference to 'exception' module to Extractor class
- remove 'exception' imports
- replace with 'self.exc'

gallery_dl/extractor/arcalive.py
@@ -7,7 +7,7 @@
 """Extractors for https://arca.live/"""
 
 from .common import Extractor, Message
-from .. import text, util, exception
+from .. import text, util
 
 BASE_PATTERN = r"(?:https?://)?(?:www\.)?arca\.live"
 
@@ -173,7 +173,7 @@ class ArcaliveAPI():
             msg = "API request failed: " + msg
         else:
             msg = "API request failed"
-        raise exception.AbortExtraction(msg)
+        raise self.exc.AbortExtraction(msg)
 
     def _pagination(self, endpoint, params, key):
         while True:

gallery_dl/extractor/bilibili.py
@@ -7,7 +7,7 @@
 """Extractors for https://www.bilibili.com/"""
 
 from .common import Extractor, Message
-from .. import text, util, exception
+from .. import text, util
 
 
 class BilibiliExtractor(Extractor):
@@ -123,7 +123,7 @@ class BilibiliAPI():
 
         if data["code"]:
             self.extractor.log.debug("Server response: %s", data)
-            raise exception.AbortExtraction("API request failed")
+            raise self.exc.AbortExtraction("API request failed")
 
         return data
 
@@ -151,7 +151,7 @@ class BilibiliAPI():
                     page, "window.__INITIAL_STATE__=", "};") + "}")
             except Exception:
                 if "window._riskdata_" not in page:
-                    raise exception.AbortExtraction(
+                    raise self.exc.AbortExtraction(
                         article_id + ": Unable to extract INITIAL_STATE data")
             self.extractor.wait(seconds=300)
 
@@ -174,9 +174,9 @@ class BilibiliAPI():
 
         if data["code"] != 0:
             self.extractor.log.debug("Server response: %s", data)
-            raise exception.AbortExtraction(
+            raise self.exc.AbortExtraction(
                 "API request failed. Are you logges in?")
         try:
             return data["data"]["profile"]["mid"]
         except Exception:
-            raise exception.AbortExtraction("API request failed")
+            raise self.exc.AbortExtraction("API request failed")

gallery_dl/extractor/bluesky.py
@@ -9,7 +9,7 @@
 """Extractors for https://bsky.app/"""
 
 from .common import Extractor, Message, Dispatch
-from .. import text, util, exception
+from .. import text, util
 from ..cache import cache, memcache
 
 BASE_PATTERN = (r"(?:https?://)?"
@@ -96,7 +96,7 @@ class BlueskyExtractor(Extractor):
                 uri = record["value"]["subject"]["uri"]
                 if "/app.bsky.feed.post/" in uri:
                     yield from self.api.get_post_thread_uri(uri, depth)
-            except exception.ControlException:
+            except self.exc.ControlException:
                 pass  # deleted post
             except Exception as exc:
                 self.log.debug(record, exc_info=exc)
@@ -569,7 +569,7 @@ class BlueskyAPI():
 
         if response.status_code != 200:
             self.log.debug("Server response: %s", data)
-            raise exception.AuthenticationError(
+            raise self.exc.AuthenticationError(
                 f"\"{data.get('error')}: {data.get('message')}\"")
 
         _refresh_token_cache.update(self.username, data["refreshJwt"])
@@ -600,7 +600,7 @@ class BlueskyAPI():
                 msg = f"{msg} ({response.status_code} {response.reason})"
 
             self.extractor.log.debug("Server response: %s", response.text)
-            raise exception.AbortExtraction(msg)
+            raise self.exc.AbortExtraction(msg)
 
     def _pagination(self, endpoint, params,
                     key="feed", root=None, check_empty=False):

gallery_dl/extractor/boosty.py
@@ -7,7 +7,7 @@
 """Extractors for https://www.boosty.to/"""
 
 from .common import Extractor, Message
-from .. import text, util, exception
+from .. import text, util
 import itertools
 
 BASE_PATTERN = r"(?:https?://)?boosty\.to"
@@ -380,14 +380,14 @@ class BoostyAPI():
                 return response.json()
 
             elif response.status_code < 400:
-                raise exception.AuthenticationError("Invalid API access token")
+                raise self.exc.AuthenticationError("Invalid API access token")
 
             elif response.status_code == 429:
                 self.extractor.wait(seconds=600)
 
             else:
                 self.extractor.log.debug(response.text)
-                raise exception.AbortExtraction("API request failed")
+                raise self.exc.AbortExtraction("API request failed")
 
     def _pagination(self, endpoint, params, transform=None, key=None):
         if "is_only_allowed" not in params and self.extractor.only_allowed:

gallery_dl/extractor/civitai.py
@@ -9,7 +9,7 @@
 """Extractors for https://www.civitai.com/"""
 
 from .common import Extractor, Message, Dispatch
-from .. import text, util, exception
+from .. import text, util
 from ..cache import memcache
 import itertools
 import time
@@ -201,7 +201,7 @@ class CivitaiExtractor(Extractor):
         if "Authorization" not in self.api.headers and \
                 not self.cookies.get(
                 "__Secure-civitai-token", domain=".civitai.com"):
-            raise exception.AuthRequired(("api-key", "authenticated cookies"))
+            raise self.exc.AuthRequired(("api-key", "authenticated cookies"))
 
     def _parse_query(self, value):
         return text.parse_query_list(

gallery_dl/extractor/deviantart.py
@@ -9,7 +9,7 @@
 """Extractors for https://www.deviantart.com/"""
 
 from .common import Extractor, Message, Dispatch
-from .. import text, util, dt, exception
+from .. import text, util, dt
 from ..cache import cache, memcache
 import collections
 import mimetypes
@@ -123,7 +123,7 @@ class DeviantartExtractor(Extractor):
                     self.group = False
                 elif group == "skip":
                     self.log.info("Skipping group '%s'", self.user)
-                    raise exception.AbortExtraction()
+                    raise self.exc.AbortExtraction()
                 else:
                     self.subcategory = "group-" + self.subcategory
                     self.group = True
@@ -457,7 +457,7 @@ class DeviantartExtractor(Extractor):
                     for subfolder in folder["subfolders"]:
                         if subfolder["folderid"] == uuid:
                             return subfolder
-        raise exception.NotFoundError("folder")
+        raise self.exc.NotFoundError("folder")
 
     def _folder_urls(self, folders, category, extractor):
         base = f"{self.root}/{self.user}/{category}/"
@@ -1027,7 +1027,7 @@ class DeviantartDeviationExtractor(DeviantartExtractor):
         page = self._limited_request(url, notfound=True).text
         uuid = text.extr(page, '"deviationUuid\\":\\"', '\\')
         if not uuid:
-            raise exception.NotFoundError("deviation")
+            raise self.exc.NotFoundError("deviation")
 
         deviation = self.api.deviation(uuid)
         deviation["_page"] = page
@@ -1111,7 +1111,7 @@ class DeviantartSearchExtractor(DeviantartExtractor):
             response = self.request(url, params=params)
 
             if response.history and "/users/login" in response.url:
-                raise exception.AbortExtraction("HTTP redirect to login page")
+                raise self.exc.AbortExtraction("HTTP redirect to login page")
             page = response.text
 
             for user, type, did in find(page)[:-3:3]:
@@ -1476,7 +1476,7 @@ class DeviantartOAuthAPI():
 
         if response.status_code != 200:
             self.log.debug("Server response: %s", data)
-            raise exception.AuthenticationError(
+            raise self.exc.AuthenticationError(
                 f"\"{data.get('error_description')}\" ({data.get('error')})")
         if refresh_token_key:
             _refresh_token_cache.update(
@@ -1515,9 +1515,9 @@ class DeviantartOAuthAPI():
 
             error = data.get("error_description")
             if error == "User not found.":
-                raise exception.NotFoundError("user or group")
+                raise self.exc.NotFoundError("user or group")
             if error == "Deviation not downloadable.":
-                raise exception.AuthorizationError()
+                raise self.exc.AuthorizationError()
 
             self.log.debug(response.text)
             msg = f"API responded with {status} {response.reason}"
@@ -1808,7 +1808,7 @@ class DeviantartEclipseAPI():
 
         pos = page.find('\\"name\\":\\"watching\\"')
         if pos < 0:
-            raise exception.NotFoundError("'watching' module ID")
+            raise self.exc.NotFoundError("'watching' module ID")
         module_id = text.rextr(page, '\\"id\\":', ',', pos).strip('" ')
 
         self._fetch_csrf_token(page)
@@ -1863,7 +1863,7 @@ def _login_impl(extr, username, password):
     response = extr.request(url, method="POST", data=data)
 
     if not response.history:
-        raise exception.AuthenticationError()
+        raise extr.exc.AuthenticationError()
 
     return {
         cookie.name: cookie.value

gallery_dl/extractor/discord.py
@@ -7,7 +7,7 @@
 """Extractors for https://discord.com/"""
 
 from .common import Extractor, Message
-from .. import text, exception
+from .. import text
 
 BASE_PATTERN = r"(?:https?://)?discord\.com"
 
@@ -167,10 +167,10 @@ class DiscordExtractor(Extractor):
                         yield from self.extract_channel(
                             channel["channel_id"], safe=True)
             elif not safe:
-                raise exception.AbortExtraction(
+                raise self.exc.AbortExtraction(
                     "This channel type is not supported."
                 )
-        except exception.HttpError as exc:
+        except self.exc.HttpError as exc:
             if not (exc.status == 403 and safe):
                 raise
 
@@ -474,7 +474,7 @@ class DiscordAPI():
         try:
             response = self.extractor.request(
                 url, params=params, headers=self.headers)
-        except exception.HttpError as exc:
+        except self.exc.HttpError as exc:
             if exc.status == 401:
                 self._raise_invalid_token()
             raise
@@ -490,7 +490,7 @@ class DiscordAPI():
             offset += len(data)
 
     def _raise_invalid_token(self):
-        raise exception.AuthenticationError("""Invalid or missing token.
+        raise self.exc.AuthenticationError("""Invalid or missing token.
 Please provide a valid token following these instructions:
 
 1) Open Discord in your browser (https://discord.com/app);

gallery_dl/extractor/imagechest.py
@@ -10,7 +10,7 @@
 """Extractors for https://imgchest.com/"""
 
 from .common import GalleryExtractor, Extractor, Message
-from .. import text, util, exception
+from .. import text, util
 
 BASE_PATTERN = r"(?:https?://)?(?:www\.)?imgchest\.com"
 
@@ -40,7 +40,7 @@ class ImagechestGalleryExtractor(GalleryExtractor):
             post = data["props"]["post"]
         except Exception:
             if "<title>Not Found</title>" in page:
-                raise exception.NotFoundError("gallery")
+                raise self.exc.NotFoundError("gallery")
             self.files = ()
             return {}
 
@@ -142,11 +142,11 @@ class ImagechestAPI():
                 return response.json()["data"]
 
             elif response.status_code < 400:
-                raise exception.AuthenticationError("Invalid API access token")
+                raise self.exc.AuthenticationError("Invalid API access token")
 
             elif response.status_code == 429:
                 self.extractor.wait(seconds=600)
 
             else:
                 self.extractor.log.debug(response.text)
-                raise exception.AbortExtraction("API request failed")
+                raise self.exc.AbortExtraction("API request failed")

commit d8880b9e66d8fd421058b11de508aaef4c490285

fix accessing 'self.exc' in …API methods
fixes regression introduced in c0eddedeae7b382dd2a29a266a499dc98a7b0c1e

gallery_dl/extractor/arcalive.py
@@ -173,7 +173,7 @@ class ArcaliveAPI():
             msg = "API request failed: " + msg
         else:
             msg = "API request failed"
-        raise self.exc.AbortExtraction(msg)
+        raise self.extractor.exc.AbortExtraction(msg)
 
     def _pagination(self, endpoint, params, key):
         while True:

gallery_dl/extractor/bilibili.py
@@ -116,6 +116,7 @@ class BilibiliUserArticlesFavoriteExtractor(BilibiliExtractor):
 class BilibiliAPI():
     def __init__(self, extractor):
         self.extractor = extractor
+        self.exc = extractor.exc
 
     def _call(self, endpoint, params):
         url = "https://api.bilibili.com/x/polymer/web-dynamic/v1" + endpoint

gallery_dl/extractor/bluesky.py
@@ -1,6 +1,6 @@
 # -*- coding: utf-8 -*-
 
-# Copyright 2024-2025 Mike Fährmann
+# Copyright 2024-2026 Mike Fährmann
 #
 # This program is free software; you can redistribute it and/or modify
 # it under the terms of the GNU General Public License version 2 as
@@ -569,7 +569,7 @@ class BlueskyAPI():
 
         if response.status_code != 200:
             self.log.debug("Server response: %s", data)
-            raise self.exc.AuthenticationError(
+            raise self.extractor.exc.AuthenticationError(
                 f"\"{data.get('error')}: {data.get('message')}\"")
 
         _refresh_token_cache.update(self.username, data["refreshJwt"])
@@ -600,7 +600,7 @@ class BlueskyAPI():
                 msg = f"{msg} ({response.status_code} {response.reason})"
 
             self.extractor.log.debug("Server response: %s", response.text)
-            raise self.exc.AbortExtraction(msg)
+            raise self.extractor.exc.AbortExtraction(msg)
 
     def _pagination(self, endpoint, params,
                     key="feed", root=None, check_empty=False):

gallery_dl/extractor/boosty.py
@@ -380,14 +380,15 @@ class BoostyAPI():
                 return response.json()
 
             elif response.status_code < 400:
-                raise self.exc.AuthenticationError("Invalid API access token")
+                raise self.extractor.exc.AuthenticationError(
+                    "Invalid API access token")
 
             elif response.status_code == 429:
                 self.extractor.wait(seconds=600)
 
             else:
                 self.extractor.log.debug(response.text)
-                raise self.exc.AbortExtraction("API request failed")
+                raise self.extractor.exc.AbortExtraction("API request failed")
 
     def _pagination(self, endpoint, params, transform=None, key=None):
         if "is_only_allowed" not in params and self.extractor.only_allowed:

gallery_dl/extractor/civitai.py
@@ -1,6 +1,6 @@
 # -*- coding: utf-8 -*-
 
-# Copyright 2024-2025 Mike Fährmann
+# Copyright 2024-2026 Mike Fährmann
 #
 # This program is free software; you can redistribute it and/or modify
 # it under the terms of the GNU General Public License version 2 as
@@ -201,7 +201,8 @@ class CivitaiExtractor(Extractor):
         if "Authorization" not in self.api.headers and \
                 not self.cookies.get(
                 "__Secure-civitai-token", domain=".civitai.com"):
-            raise self.exc.AuthRequired(("api-key", "authenticated cookies"))
+            raise self.extractor.exc.AuthRequired(
+                ("api-key", "authenticated cookies"))
 
     def _parse_query(self, value):
         return text.parse_query_list(

gallery_dl/extractor/deviantart.py
@@ -1187,6 +1187,7 @@ class DeviantartOAuthAPI():
     def __init__(self, extractor):
         self.extractor = extractor
         self.log = extractor.log
+        self.exc = extractor.exc
         self.headers = {"dA-minor-version": "20210526"}
         self._warn_429 = True
 
@@ -1808,7 +1809,7 @@ class DeviantartEclipseAPI():
 
         pos = page.find('\\"name\\":\\"watching\\"')
         if pos < 0:
-            raise self.exc.NotFoundError("'watching' module ID")
+            raise self.extractor.exc.NotFoundError("'watching' module ID")
         module_id = text.rextr(page, '\\"id\\":', ',', pos).strip('" ')
 
         self._fetch_csrf_token(page)

gallery_dl/extractor/discord.py
@@ -490,7 +490,8 @@ class DiscordAPI():
             offset += len(data)
 
     def _raise_invalid_token(self):
-        raise self.exc.AuthenticationError("""Invalid or missing token.
+        raise self.extractor.exc.AuthenticationError("""\
+Invalid or missing token.
 Please provide a valid token following these instructions:
 
 1) Open Discord in your browser (https://discord.com/app);

gallery_dl/extractor/imagechest.py
@@ -1,7 +1,7 @@
 # -*- coding: utf-8 -*-
 
 # Copyright 2020 Leonid "Bepis" Pavel
-# Copyright 2023-2025 Mike Fährmann
+# Copyright 2023-2026 Mike Fährmann
 #
 # This program is free software; you can redistribute it and/or modify
 # it under the terms of the GNU General Public License version 2 as
@@ -142,11 +142,12 @@ class ImagechestAPI():
                 return response.json()["data"]
 
             elif response.status_code < 400:
-                raise self.exc.AuthenticationError("Invalid API access token")
+                raise self.extractor.exc.AuthenticationError(
+                    "Invalid API access token")
 
             elif response.status_code == 429:
                 self.extractor.wait(seconds=600)
 
             else:
                 self.extractor.log.debug(response.text)
-                raise self.exc.AbortExtraction("API request failed")
+                raise self.extractor.exc.AbortExtraction("API request failed")

gallery_dl/extractor/inkbunny.py
@@ -1,6 +1,6 @@
 # -*- coding: utf-8 -*-
 
-# Copyright 2020-2025 Mike Fährmann
+# Copyright 2020-2026 Mike Fährmann
 #
 # This program is free software; you can redistribute it and/or modify
 # it under the terms of the GNU General Public License version 2 as
@@ -348,7 +348,7 @@ class InkbunnyAPI():
                 self.authenticate(invalidate=True)
                 continue
 
-            raise self.exc.AbortExtraction(data.get("error_message"))
+            raise self.extractor.exc.AbortExtraction(data.get("error_message"))
 
     def _pagination_search(self, params):
         params["page"] = 1

gallery_dl/extractor/instagram.py
@@ -804,6 +804,7 @@ class InstagramRestAPI():
 
     def __init__(self, extractor):
         self.extractor = extractor
+        self.exc = extractor.exc
 
         _cache = self.extractor.config("user-cache", True)
         _cache = memcache if not _cache or _cache == "memory" else cache
@@ -1087,7 +1088,8 @@ class InstagramGraphqlAPI():
         self.user_id = api.user_id
 
     def _unsupported(self, _=None):
-        raise self.exc.AbortExtraction("Unsupported with GraphQL API")
+        raise self.extractor.exc.AbortExtraction(
+            "Unsupported with GraphQL API")
 
     def highlights_tray(self, user_id):
         query_hash = "d4d88dc1500312af6f937f7b804c68c3"

gallery_dl/extractor/iwara.py
@@ -297,6 +297,7 @@ class IwaraAPI():
 
     def __init__(self, extractor):
         self.extractor = extractor
+        self.exc = extractor.exc
         self.headers = {
             "Referer"     : extractor.root + "/",
             "Content-Type": "application/json",

gallery_dl/extractor/lofter.py
@@ -132,11 +132,11 @@ class LofterAPI():
         info = response.json()
 
         if info["meta"]["status"] == 4200:
-            raise self.exc.NotFoundError("blog")
+            raise self.extractor.exc.NotFoundError("blog")
 
         if info["meta"]["status"] != 200:
             self.extractor.log.debug("Server response: %s", info)
-            raise self.exc.AbortExtraction("API request failed")
+            raise self.extractor.exc.AbortExtraction("API request failed")
 
         return info["response"]
 

gallery_dl/extractor/mangadex.py
@@ -1,6 +1,6 @@
 # -*- coding: utf-8 -*-
 
-# Copyright 2018-2025 Mike Fährmann
+# Copyright 2018-2026 Mike Fährmann
 #
 # This program is free software; you can redistribute it and/or modify
 # it under the terms of the GNU General Public License version 2 as
@@ -333,7 +333,8 @@ class MangadexAPI():
         try:
             access_token = data["access_token"]
         except Exception:
-            raise self.exc.AuthenticationError(data.get("error_description"))
+            raise self.extractor.exc.AuthenticationError(
+                data.get("error_description"))
 
         if refresh_token != data.get("refresh_token"):
             _refresh_token_cache.update(
@@ -356,7 +357,7 @@ class MangadexAPI():
         data = self.extractor.request_json(
             url, method="POST", json=json, fatal=None)
         if data.get("result") != "ok":
-            raise self.exc.AuthenticationError()
+            raise self.extractor.exc.AuthenticationError()
 
         if refresh_token != data["token"]["refresh"]:
             _refresh_token_cache.update(username, data["token"]["refresh"])
@@ -381,7 +382,7 @@ class MangadexAPI():
 
             msg = ", ".join(f'{error["title"]}: "{error["detail"]}"'
                             for error in response.json()["errors"])
-            raise self.exc.AbortExtraction(
+            raise self.extractor.exc.AbortExtraction(
                 f"{response.status_code} {response.reason} ({msg})")
 
     def _pagination_chapters(self, endpoint, params=None, auth=False):

gallery_dl/extractor/mastodon.py
@@ -1,6 +1,6 @@
 # -*- coding: utf-8 -*-
 
-# Copyright 2019-2025 Mike Fährmann
+# Copyright 2019-2026 Mike Fährmann
 #
 # This program is free software; you can redistribute it and/or modify
 # it under the terms of the GNU General Public License version 2 as
@@ -215,6 +215,7 @@ class MastodonAPI():
     def __init__(self, extractor):
         self.root = extractor.root
         self.extractor = extractor
+        self.exc = extractor.exc
 
         access_token = extractor.config("access-token")
         if access_token is None or access_token == "cache":

gallery_dl/extractor/misskey.py
@@ -239,7 +239,7 @@ class MisskeyAPI():
     def i_favorites(self):
         endpoint = "/i/favorites"
         if not self.access_token:
-            raise self.exc.AuthenticationError()
+            raise self.extractor.exc.AuthenticationError()
         data = {"i": self.access_token}
         return self._pagination(endpoint, data)
 

gallery_dl/extractor/pexels.py
@@ -1,6 +1,6 @@
 # -*- coding: utf-8 -*-
 
-# Copyright 2025 Mike Fährmann
+# Copyright 2025-2026 Mike Fährmann
 #
 # This program is free software; you can redistribute it and/or modify
 # it under the terms of the GNU General Public License version 2 as
@@ -174,7 +174,7 @@ class PexelsAPI():
 
             else:
                 self.extractor.log.debug(response.text)
-                raise self.exc.AbortExtraction("API request failed")
+                raise self.extractor.exc.AbortExtraction("API request failed")
 
     def _pagination(self, endpoint, params):
         while True:

gallery_dl/extractor/philomena.py
@@ -1,6 +1,6 @@
 # -*- coding: utf-8 -*-
 
-# Copyright 2021-2025 Mike Fährmann
+# Copyright 2021-2026 Mike Fährmann
 #
 # This program is free software; you can redistribute it and/or modify
 # it under the terms of the GNU General Public License version 2 as
@@ -159,7 +159,7 @@ class PhilomenaAPI():
 
             # error
             self.extractor.log.debug(response.content)
-            raise self.exc.HttpError("", response)
+            raise self.extractor.exc.HttpError("", response)
 
     def _pagination(self, endpoint, params):
         extr = self.extractor

gallery_dl/extractor/pinterest.py
@@ -1,6 +1,6 @@
 # -*- coding: utf-8 -*-
 
-# Copyright 2016-2025 Mike Fährmann
+# Copyright 2016-2026 Mike Fährmann
 #
 # This program is free software; you can redistribute it and/or modify
 # it under the terms of the GNU General Public License version 2 as
@@ -545,9 +545,9 @@ class PinterestAPI():
             return data
         if response.status_code == 404:
             resource = self.extractor.subcategory.rpartition("-")[2]
-            raise self.exc.NotFoundError(resource)
+            raise self.extractor.exc.NotFoundError(resource)
         self.extractor.log.debug("Server response: %s", response.text)
-        raise self.exc.AbortExtraction("API request failed")
+        raise self.extractor.exc.AbortExtraction("API request failed")
 
     def _pagination(self, resource, options):
         while True:

gallery_dl/extractor/pixiv.py
@@ -1123,6 +1123,7 @@ class PixivAppAPI():
     def __init__(self, extractor):
         self.extractor = extractor
         self.log = extractor.log
+        self.exc = extractor.exc
         self.username = extractor._get_auth_info()[0]
         self.user = None
 

gallery_dl/extractor/reddit.py
@@ -525,7 +525,7 @@ class RedditAPI():
 
         if response.status_code != 200:
             self.log.debug("Server response: %s", data)
-            raise self.exc.AuthenticationError(
+            raise self.extractor.exc.AuthenticationError(
                 f"\"{data.get('error')}: {data.get('message')}\"")
         return "Bearer " + data["access_token"]
 
@@ -555,16 +555,17 @@ class RedditAPI():
             try:
                 data = response.json()
             except ValueError:
-                raise self.exc.AbortExtraction(
+                raise self.extractor.exc.AbortExtraction(
                     text.remove_html(response.text))
 
             if "error" in data:
+                exc = self.extractor.exc
                 if data["error"] == 403:
-                    raise self.exc.AuthorizationError()
+                    raise exc.AuthorizationError()
                 if data["error"] == 404:
-                    raise self.exc.NotFoundError()
+                    raise exc.NotFoundError()
                 self.log.debug(data)
-                raise self.exc.AbortExtraction(data.get("message"))
+                raise exc.AbortExtraction(data.get("message"))
             return data
 
     def _pagination(self, endpoint, params):
@@ -592,7 +593,7 @@ class RedditAPI():
                         if post["num_comments"] and self.comments:
                             try:
                                 yield self.submission(post["id"])
-                            except self.exc.AuthorizationError:
+                            except self.extractor.exc.AuthorizationError:
                                 pass
                         else:
                             yield post, ()

gallery_dl/extractor/sankaku.py
@@ -1,6 +1,6 @@
 # -*- coding: utf-8 -*-
 
-# Copyright 2014-2025 Mike Fährmann
+# Copyright 2014-2026 Mike Fährmann
 #
 # This program is free software; you can redistribute it and/or modify
 # it under the terms of the GNU General Public License version 2 as
@@ -284,7 +284,7 @@ class SankakuAPI():
             if response.status_code == 429:
                 until = response.headers.get("X-RateLimit-Reset")
                 if not until and b"_tags-explicit-limit" in response.content:
-                    raise self.exc.AuthorizationError(
+                    raise self.extractor.exc.AuthorizationError(
                         "Search tag limit exceeded")
                 seconds = None if until else 600
                 self.extractor.wait(until=until, seconds=seconds)
@@ -305,7 +305,7 @@ class SankakuAPI():
                     code = f"'{code.rpartition('__')[2].replace('-', ' ')}'"
                 except Exception:
                     pass
-                raise self.exc.AbortExtraction(code)
+                raise self.extractor.exc.AbortExtraction(code)
             return data
 
     def _pagination(self, endpoint, params):

gallery_dl/extractor/twibooru.py
@@ -1,6 +1,6 @@
 # -*- coding: utf-8 -*-
 
-# Copyright 2022-2025 Mike Fährmann
+# Copyright 2022-2026 Mike Fährmann
 #
 # This program is free software; you can redistribute it and/or modify
 # it under the terms of the GNU General Public License version 2 as
@@ -153,7 +153,7 @@ class TwibooruAPI():
 
             # error
             self.extractor.log.debug(response.content)
-            raise self.exc.HttpError("", response)
+            raise self.extractor.exc.HttpError("", response)
 
     def _pagination(self, endpoint, params):
         extr = self.extractor

gallery_dl/extractor/twitter.py
@@ -1247,6 +1247,7 @@ class TwitterAPI():
     def __init__(self, extractor):
         self.extractor = extractor
         self.log = extractor.log
+        self.exc = extractor.exc
 
         self.root = "https://x.com/i/api"
         self._nsfw_warning = True

gallery_dl/extractor/wallhaven.py
@@ -1,6 +1,6 @@
 # -*- coding: utf-8 -*-
 
-# Copyright 2018-2025 Mike Fährmann
+# Copyright 2018-2026 Mike Fährmann
 #
 # This program is free software; you can redistribute it and/or modify
 # it under the terms of the GNU General Public License version 2 as
@@ -199,7 +199,7 @@ class WallhavenAPI():
                 continue
 
             self.extractor.log.debug("Server response: %s", response.text)
-            raise self.exc.AbortExtraction(
+            raise self.extractor.exc.AbortExtraction(
                 f"API request failed "
                 f"({response.status_code} {response.reason})")
 

gallery_dl/oauth.py
@@ -1,6 +1,6 @@
 # -*- coding: utf-8 -*-
 
-# Copyright 2018-2020 Mike Fährmann
+# Copyright 2018-2026 Mike Fährmann
 #
 # This program is free software; you can redistribute it and/or modify
 # it under the terms of the GNU General Public License version 2 as
@@ -109,6 +109,7 @@ class OAuth1API():
     API_SECRET = None
 
     def __init__(self, extractor):
+        self.exc = extractor.exc
         self.log = extractor.log
         self.extractor = extractor
 